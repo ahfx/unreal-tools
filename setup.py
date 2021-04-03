22eff1010645943f8e3a905c8a22f96f6de3d507
Adam Harder <bj.adam.harder@gmail.com>
2021-04-03 11:36:31 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='unreal-tools',
+      version='0.1',
+      packages=['unreal_tools'],
+)
---
+++ unreal_tools/__init__.py
@@ -0,0 +1,5 @@
+def hello_world():
+	print("hello_world from unreal_tools")
+
+
+hello_world()
